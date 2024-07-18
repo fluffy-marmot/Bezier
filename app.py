1fc9dc8805d3b762f6f51d9fcb8dfcba66767888
fluffy-marmot <10621013+fluffy-marmot@users.noreply.github.com>
2024-07-18 12:37:46 -0700

    added files

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+# from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
