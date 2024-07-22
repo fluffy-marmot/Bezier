39b8693102281abd2afacca8039476b9d8ecac36
fluffy-marmot <10621013+fluffy-marmot@users.noreply.github.com>
2024-07-21 21:54:17 -0700

    ui rework and more cleanup

--- static/mathy.js
+++ static/mathy.js
@@ -1,6 +1,16 @@
 const BACKGROUND_COLOR = '#2A383E';
+const BERNSTEIN_BOX_COLOR_FUNCS = ['magenta', 'royalblue', 'springgreen', 'yellow'];
+const BERNSTEIN_BOX_COLOR_BCKG = 'black';
+const BERNSTEIN_BOX_COLOR_BORDER = 'white';
 
 const BEZIER_COLOR = 'red';
+const BEZIER_COLOR_ACTIVE = 'tomato';
+// const BEZIER_COLOR_LERPS = ['chocolate', 'coral', 'bisque'];
+const BEZIER_COLOR_LERPS = ['#C9CC3F', '#C9CC3F', '#C9CC3F'];
+const BEZIER_COLOR_BOUNDS = 'blue';
+const BEZIER_COLOR_TANGENT = '#FF47A6';
+const BEZIER_COLOR_NORMAL = '#B6FF72';
+const BEZIER_COLOR_OSCULATING_CIRCLE = 'white';
 
 const POINT_DRAW_RADIUS = 3;
 
@@ -8,21 +18,19 @@ const POINT_STANDARD = 0;
 const POINT_MOVING = 1;
 const POINT_FORMING_BEZIER = 2;
 const POINT_ORIGIN = 3;
+const POINT_ACTIVE = 4;
 
-const POINT_COLORS = ['yellow', 'red', 'orange', 'black']
-const POINT_COLORS_BORDER = ['yellow', 'red', 'orange', 'white'];
+const POINT_COLORS = ['yellow', 'red', 'orange', 'black', 'red']
+const POINT_COLORS_BORDER = ['yellow', 'red', 'orange', 'white', BEZIER_COLOR_ACTIVE];
 
 
 class Point {
 
     constructor(x, y) {
         this.move(x, y);
-
+        this.moving = false;
         this.radius = POINT_DRAW_RADIUS;
         this.mode = POINT_STANDARD;
-
-        this.visible = true;
-        this.moving = false;
     }
 
     move(x, y) {
@@ -33,13 +41,18 @@ class Point {
     }
 
     draw(ctx) {
-        ctx.fillStyle = POINT_COLORS[this.mode];
-        ctx.strokeStyle = POINT_COLORS_BORDER[this.mode];
+        if ('color' in this) {
+            ctx.fillStyle = 'color';
+        } else {
+            ctx.fillStyle = POINT_COLORS[this.mode];
+            ctx.strokeStyle = POINT_COLORS_BORDER[this.mode];
+        }
 
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
-        ctx.stroke();
+        if (!('color' in this))
+            ctx.stroke();
     }
 }
 
@@ -50,21 +63,6 @@ class CubicBezier {
         if (pts.length == 4 && pts[0] instanceof Point && pts[1] instanceof Point && pts[2] instanceof Point && pts[3] instanceof Point) {
             this.points = pts;
             this.calc_center();
-            this.box = this.calculate_bounds();
-
-            this.color = BEZIER_COLOR;
-            this.mode = POINT_STANDARD;
-            this.visible = true;
-
-            this.show_lerps = false;
-            this.show_handles = false;
-            this.show_box = false;
-            this.show_tangent = true;
-            this.show_normal = true;
-            this.show_lc = true;
-            this.show_osculating = true;
-            this.color_lerps = ['chocolate', 'coral', 'bisque'];
-            this.t_param = 0.5;
         } else {
             throw new Error("CubicBezier constructor: curve must be defined by 4 points");
         }
@@ -109,7 +107,7 @@ class CubicBezier {
     }
 
 
-    calculate_bounds() {
+    calc_bounds() {
 
         function quadratic_formula(a, b, c) {
             let discriminant = b*b - 4*a*c;
@@ -144,15 +142,11 @@ class CubicBezier {
                 new Point(Math.max(...potential_x), Math.max(...potential_y))];
     }
 
-    draw(ctx) {
-        for (let point of this.points) {
+    draw(ctx, flags, active) {
+        for (let point of this.points)
             point.draw(ctx);
-        }
 
-        ctx.strokeStyle = this.color;
-        if (this.show_lerps || this.show_tangent || this.show_lc) {
-            ctx.strokeStyle = 'tomato';
-        }
+        ctx.strokeStyle = active ? BEZIER_COLOR_ACTIVE : BEZIER_COLOR;
         ctx.lineWidth = 2;
         ctx.beginPath();
         ctx.moveTo(this.points[0].x, this.points[0].y);
@@ -160,137 +154,143 @@ class CubicBezier {
         ctx.stroke();
         ctx.lineWidth = 1;
 
-        let cur_t = this.calc_bernstein(this.t_param);
-
-        if (this.show_lerps) {
-            this.draw_lerps(this.points, this.t_param, this.color_lerps, ctx);
-        } else if (this.show_handles) {
+        if (flags.show_handles)
             this.draw_handles(ctx);
-        }
-        if (this.show_lerps || this.show_tangent || this.show_normal || this.show_lc || this.show_osculating) {
-            cur_t.color = 'red';
-            cur_t.radius += 1;
-            cur_t.draw(ctx);
-        }
-        if (this.show_box) {
-            let bounds = this.calculate_bounds();
-            ctx.globalAlpha = 0.5;
-            ctx.strokeStyle = 'blue';
-            ctx.strokeRect(bounds[0].x, bounds[0].y, bounds[1].x - bounds[0].x, bounds[1].y - bounds[0].y);
-            ctx.fillStyle = 'blue';
-            ctx.globalAlpha = 0.03;
-            ctx.fillRect(bounds[0].x, bounds[0].y, bounds[1].x - bounds[0].x, bounds[1].y - bounds[0].y);
-            ctx.globalAlpha = 1.0;
-        }
-        if (this.show_tangent || this.show_normal) {
-            let deriv = this.calc_bernstein_derivative(this.t_param);
-            let der2 = this.calc_bernstein_second_derivative(this.t_param);
 
-            if (this.show_tangent) {
-                drawArrow(cur_t.x, cur_t.y, cur_t.x + deriv.x / 5, cur_t.y + deriv.y / 5, ctx, '#FF47A6');
-            }
+        if (active) {
+            if (flags['show_lerps'])
+                this.draw_lerps(ctx, this.points, flags.t_param, BEZIER_COLOR_LERPS.slice());
 
-            if (this.show_normal) {
-                if (deriv.y * der2.x - deriv.x * der2.y >= 0) {
-                    drawArrow(cur_t.x, cur_t.y, cur_t.x + deriv.y / 5, cur_t.y - deriv.x / 5, ctx, '#B6FF72');
+            let P = this.calc_bernstein(flags.t_param);
+            let dP = this.calc_bernstein_derivative(flags.t_param);
+            let ddP = this.calc_bernstein_second_derivative(flags.t_param);
+
+            P.mode = POINT_ACTIVE;
+            P.radius += 1;
+            P.draw(ctx);
+            
+            if (flags['show_box'])
+                this.draw_bounds(ctx);
+
+            if (flags['show_lc'])
+                this.draw_lc(ctx, flags.t_param);
+
+            if (flags['show_tangent'])
+                drawArrow(P.x, P.y, P.x + dP.x / 5, P.y + dP.y / 5, ctx, BEZIER_COLOR_TANGENT);
+
+            if (flags['show_normal']) {
+                if (dP.y * ddP.x - dP.x * ddP.y >= 0) {
+                    drawArrow(P.x, P.y, P.x + dP.y / 5, P.y - dP.x / 5, ctx, BEZIER_COLOR_NORMAL);
                 } else {
-                    drawArrow(cur_t.x, cur_t.y, cur_t.x - deriv.y / 5, cur_t.y + deriv.x / 5, ctx, '#B6FF72');
+                    drawArrow(P.x, P.y, P.x - dP.y / 5, P.y + dP.x / 5, ctx, BEZIER_COLOR_NORMAL);
                 }
             }
 
-            // drawArrow(cur_t.x, cur_t.y, cur_t.x + der2.x / 5, cur_t.y + der2.y / 5, context, 'red' ) 2nd der vector for testing
-        }
-        if (this.show_lc) {
-            this.origin.draw(ctx);
+            if (flags['show_osculating']) {
+                let d_len = Math.hypot(dP.x, dP.y);
+                let r = ((dP.x * dP.x) + (dP.y * dP.y))**(1.5) / Math.abs(dP.x * ddP.y - dP.y * ddP.x);
 
-            // draw dotted vectors between "origin" and curve control points
-            for (let i = 0; i < this.points.length; i++) {
-                drawArrow(
-                    this.origin.x, this.origin.y,
-                    this.points[i].x, this.points[i].y,
-                    ctx, BernsteinBox.colors[i], 2, 0.35, [3, 5], 10
-                );
-            }
+                let normal = new Point(dP.y / d_len * r, -dP.x / d_len * r);
 
-            let start_x = this.origin.x;
-            let start_y = this.origin.y;
+                if (normal.x * ddP.x + normal.y * ddP.y < 0) {
+                    normal.x *= -1;
+                    normal.y *= -1;
+                }
+                let center_x = P.x + normal.x, center_y = P.y + normal.y;
 
-            for (let i = 0; i < this.points.length; i++) {
-                let mult = BernsteinBox.functions[i](this.t_param);
-                let dx = (this.points[i].x - this.origin.x) * mult;
-                let dy = (this.points[i].y - this.origin.y) * mult;
-                drawArrow(start_x, start_y, start_x + dx, start_y + dy,
-                    ctx, BernsteinBox.colors[i], 3, 1.0, [], 5);
-                start_x = start_x + dx;
-                start_y = start_y + dy;
+                drawArrow(P.x, P.y, center_x, center_y, ctx, BEZIER_COLOR_OSCULATING_CIRCLE, 1, 0.5, [2, 5], 3);
+
+                ctx.strokeStyle = BEZIER_COLOR_OSCULATING_CIRCLE;
+                ctx.lineWidth = 2;
+                ctx.beginPath();
+                ctx.arc(center_x, center_y, r, 0, Math.PI * 2);
+                ctx.stroke();
+
+                ctx.lineWidth = 1;
             }
         }
-        if (this.show_osculating) {
-            let d = this.calc_bernstein_derivative(this.t_param);
-            let d2 = this.calc_bernstein_second_derivative(this.t_param);
-
-            let d_len = Math.hypot(d.x, d.y);
-            let r = ((d.x * d.x) + (d.y * d.y))**(1.5) / Math.abs(d.x * d2.y - d.y * d2.x);
-
-            let normal = new Point(d.y / d_len * r, -d.x / d_len * r);
-
-            if (normal.x * d2.x + normal.y * d2.y < 0) {
-                normal.x *= -1;
-                normal.y *= -1;
-            }
-            let center_x = cur_t.x + normal.x, center_y = cur_t.y + normal.y;
-
-            drawArrow(cur_t.x, cur_t.y, center_x, center_y, ctx, 'white', 1, 0.5, [2, 5], 3);
-
-            ctx.strokeStyle = 'white';
-            ctx.lineWidth = 2;
-            ctx.beginPath();
-            ctx.arc(center_x, center_y, r, 0, Math.PI * 2);
-            ctx.stroke();
-
-            ctx.lineWidth = 1;
-        }
-
     }
+    
+    /*  Recursively draws linear interpolations between a set of points for a given t parameter, 
+        each successive call uses a different color from the colors array */
 
-    draw_handles(context) {
-        context.strokeStyle = this.color_lerps[0];
-        context.globalAlpha = 0.5;
-        for (let a of [[0, 1], [2, 3]]) {
-            context.beginPath();
-            context.moveTo(this.points[a[0]].x, this.points[a[0]].y);
-            context.lineTo(this.points[a[1]].x, this.points[a[1]].y);
-            context.stroke();
-        }
-        context.globalAlpha = 1.0;
-    }
+    draw_lerps(ctx, pts, t, colors) {
         
-    draw_lerps(pts, t, colors, context) {
-
         function lerp(A, B, t) {
             return new Point(A.x * (1 - t) + B.x * t, A.y * (1 - t) + B.y * t);
         }
 
-        context.strokeStyle = colors[0];
+        ctx.strokeStyle = colors[0];
         let lerp_points = [];
         for (let i = 0; i < pts.length - 1; i++) {
-            context.beginPath();
-            context.moveTo(pts[i].x, pts[i].y);
-            context.lineTo(pts[i + 1].x, pts[i + 1].y);
-            context.stroke();
+            ctx.beginPath();
+            ctx.moveTo(pts[i].x, pts[i].y);
+            ctx.lineTo(pts[i + 1].x, pts[i + 1].y);
+            ctx.stroke();
 
             let lerp_pt = lerp(pts[i], pts[i + 1], t);
-            lerp_pt.color = context.strokeStyle;
+            lerp_pt.color = ctx.strokeStyle;
             lerp_pt.radius -= 1;
-            lerp_pt.draw(context);
+            lerp_pt.draw(ctx);
             lerp_points.push(lerp_pt);
         }
         if (lerp_points.length > 1) {
-            this.draw_lerps(lerp_points, t, colors.slice(1), context)
+            this.draw_lerps(ctx, lerp_points, t, colors.slice(1))
+        }
+    }
+
+    draw_handles(ctx) {
+        ctx.strokeStyle = BEZIER_COLOR_LERPS[0];
+        ctx.globalAlpha = 0.5;
+        for (let a of [[0, 1], [2, 3]]) {
+            ctx.beginPath();
+            ctx.moveTo(this.points[a[0]].x, this.points[a[0]].y);
+            ctx.lineTo(this.points[a[1]].x, this.points[a[1]].y);
+            ctx.stroke();
+        }
+        ctx.globalAlpha = 1.0;
+    }
+
+    draw_bounds(ctx) {
+        let bounds = this.calc_bounds();
+        ctx.globalAlpha = 0.5;
+        ctx.strokeStyle = BEZIER_COLOR_BOUNDS;
+        ctx.strokeRect(bounds[0].x, bounds[0].y, bounds[1].x - bounds[0].x, bounds[1].y - bounds[0].y);
+        ctx.fillStyle = BEZIER_COLOR_BOUNDS;
+        ctx.globalAlpha = 0.03;
+        ctx.fillRect(bounds[0].x, bounds[0].y, bounds[1].x - bounds[0].x, bounds[1].y - bounds[0].y);
+        ctx.globalAlpha = 1.0;
+    }
+
+    draw_lc(ctx, t) {
+        this.origin.draw(ctx);
+
+        // draw dotted vectors between "origin" and curve control points
+        for (let i = 0; i < this.points.length; i++) {
+            drawArrow(
+                this.origin.x, this.origin.y,
+                this.points[i].x, this.points[i].y,
+                ctx, BERNSTEIN_BOX_COLOR_FUNCS[i], 2, 0.35, [3, 5], 10
+            );
+        }
+
+        let start_x = this.origin.x, start_y = this.origin.y;
+
+        for (let i = 0; i < this.points.length; i++) {
+            let mult = BernsteinBox.functions[i](t);
+            let dx = (this.points[i].x - this.origin.x) * mult;
+            let dy = (this.points[i].y - this.origin.y) * mult;
+            drawArrow(start_x, start_y, start_x + dx, start_y + dy,
+                ctx, BERNSTEIN_BOX_COLOR_FUNCS[i], 3, 1.0, [], 5);
+            start_x = start_x + dx;
+            start_y = start_y + dy;
         }
     }
 }
 
+
+/*  Used enforce C1 continuity between the different cubic bezier that make up a Spline curve.
+    The control points of adjacent curves must be symmetric over their shared point */
 
 class MirrorConstraint {
 
@@ -333,12 +333,12 @@ class MirrorConstraint {
     }
 }
 
+
 class Spline {
 
     constructor() {
         this.curves = [];
         this.constraints = [];
-        this.active = true; // actively adding new curves to this spline
         this.continuity = 1;
         this.next_curve_points = 4;
     }
@@ -375,83 +375,79 @@ class Spline {
     }
 
     check_constraints() {
-        for (let c of this.constraints) {
+        for (let c of this.constraints)
             c.check_changes();
-        }
     }
 
-    draw(context) {
-        for (let curve of this.curves) {
-            curve.draw(context);
-        }
+    draw(ctx, flags, active) {
+        for (let i = 0; i < this.curves.length; i++)
+            this.curves[i].draw(ctx, flags, i === active);
     }
 }
 
+
 class BernsteinBox {
     static functions = [
-        function(t) {return -(t**3) + 3*(t**2) - 3*t + 1;},
-        function(t) {return 3*(t**3) - 6*(t**2) + 3*t;},
-        function(t) {return -3*(t**3) + 3*(t**2);},
-        function(t) {return t**3;}
+        function(t) {return ((-t + 3)*t - 3)*t + 1;},
+        function(t) {return ((3*t - 6)*t + 3)*t;},
+        function(t) {return ((-3*t + 3)*t)*t;},
+        function(t) {return t*t*t;}
     ];
-
-    static colors = ['magenta', 'royalblue', 'springgreen', 'yellow'];
 
     constructor() {
         this.width = 150;
         this.height = 150;
         this.padding = 30;
-        this.t_param = 0.0;
     }
 
-    draw(context) {
-        context.fillStyle = 'black';
-        context.fillRect(
-            context.canvas.width - this.width - this.padding,
-            context.canvas.height - this.height - this.padding, this.width, this.height);
+    draw(ctx, flags) {
+        ctx.fillStyle = BERNSTEIN_BOX_COLOR_BCKG;
+        ctx.fillRect(
+            ctx.canvas.width - this.width - this.padding,
+            ctx.canvas.height - this.height - this.padding, this.width, this.height);
 
         for(let i = 0; i < BernsteinBox.functions.length; i++) {
-            this.draw_function(context, BernsteinBox.colors[i], BernsteinBox.functions[i]);
+            this.draw_function(ctx, BERNSTEIN_BOX_COLOR_FUNCS[i], BernsteinBox.functions[i]);
         }
         for (let i = 0; i < BernsteinBox.functions.length; i++) {
-            let x = this.ctx_x(this.t_param);
-            let y = this.ctx_y(BernsteinBox.functions[i](this.t_param));
+            let x = this.ctx_x(ctx, flags.t_param);
+            let y = this.ctx_y(ctx, BernsteinBox.functions[i](flags.t_param));
 
-            context.fillStyle = 'black';
-            context.beginPath();
-            context.arc(x, y, 3, 0, Math.PI * 2);
-            context.fill();
-            context.strokeStyle = BernsteinBox.colors[i];
-            // context.arc(x, y, 4, 0, Math.PI * 2);
-            context.stroke();
+            ctx.fillStyle = BERNSTEIN_BOX_COLOR_BCKG
+            ctx.beginPath();
+            ctx.arc(x, y, 3, 0, Math.PI * 2);
+            ctx.fill();
+            ctx.strokeStyle = BERNSTEIN_BOX_COLOR_FUNCS[i];
+            ctx.stroke();
 
         }
 
-        context.strokeStyle = 'white';
-        context.strokeRect(
-            context.canvas.width - this.width - this.padding,
-            context.canvas.height - this.height - this.padding, this.width, this.height);
+        ctx.strokeStyle = BERNSTEIN_BOX_COLOR_BORDER;
+        ctx.strokeRect(
+            ctx.canvas.width - this.width - this.padding,
+            ctx.canvas.height - this.height - this.padding, this.width, this.height);
+    }
+
+    draw_function(ctx, color, func) {
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(this.ctx_x(ctx, 0), this.ctx_y(ctx, func(0)));
+        for (let pixelX = this.ctx_x(ctx, 0) + 1; pixelX <= this.ctx_x(ctx, 1); pixelX++) {
+            ctx.lineTo(pixelX, this.ctx_y(ctx, func(this.func_x(ctx, pixelX))));
+        }
+        ctx.stroke();
     }
 
      /*  Helper functions to convert between pixel coords on the drawing context and
         Bernstein polynomial function values shown on the graph */
-    func_x(ctx_x) {
-        return (ctx_x - context.canvas.width + this.width + this.padding) / this.width;
-    }
-    ctx_x(x) {
-        return context.canvas.width - this.width - this.padding + this.width * x;
-    }
-    ctx_y(y) {
-        return context.canvas.height - this.height * y - this.padding;
-    }
 
-    draw_function(context, color, func) {
-        context.strokeStyle = color;
-        context.beginPath();
-        context.moveTo(this.ctx_x(0), this.ctx_y(func(0)));
-        for (let pixelX = this.ctx_x(0) + 1; pixelX <= this.ctx_x(1); pixelX++) {
-            context.lineTo(pixelX, this.ctx_y(func(this.func_x(pixelX))));
-        }
-        context.stroke();
+    func_x(ctx, ctx_x) {
+        return (ctx_x - ctx.canvas.width + this.width + this.padding) / this.width;
+    }
+    ctx_x(ctx, x) {
+        return ctx.canvas.width - this.width - this.padding + this.width * x;
+    }
+    ctx_y(ctx, y) {
+        return ctx.canvas.height - this.height * y - this.padding;
     }
 }
